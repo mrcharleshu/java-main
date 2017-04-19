445788014c4d2f6253cece7595838ddff47f40aa
Charles <hub@pukkasoft.cn>
2017-04-19 11:27:16 +0800

    修改测试类

--- src/main/java/com/charles/utils/MD5.java
+++ src/main/java/com/charles/utils/MD5.java
@@ -1,19 +1,47 @@
 package com.charles.utils;
 
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 public class MD5 {
 
+    private static final char MD5_CHARS[]
+            = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
     public String getMD5(String source) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("MD5");
+        Objects.requireNonNull(source);
+        //  MessageDigest md = MessageDigest.getInstance("MD5");
         // 计算md5函数
-        md.update(source.getBytes());
+        // md.update(source.getBytes());
         // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
         // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-        return new BigInteger(1, md.digest()).toString(16);
+        // return new BigInteger(1, md.digest()).toString(16);
 
+        //用于加密的字符
+        //使用平台的默认字符集将此 String 编码为 byte序列，并将结果存储到一个新的 byte数组中
+        byte[] btInput = source.getBytes();
+
+        //信息摘要是安全的单向哈希函数，它接收任意大小的数据，并输出固定长度的哈希值。
+        MessageDigest mdInst = MessageDigest.getInstance("MD5");
+
+        //MessageDigest对象通过使用 update方法处理数据， 使用指定的byte数组更新摘要
+        mdInst.update(btInput);
+
+        // 摘要更新之后，通过调用digest（）执行哈希计算，获得密文
+        byte[] md = mdInst.digest();
+
+        // 把密文转换成十六进制的字符串形式
+        int j = md.length;
+        char str[] = new char[j * 2];
+        int k = 0;
+        for (int i = 0; i < j; i++) {   //  i = 0
+            byte byte0 = md[i];  //95
+            str[k++] = MD5_CHARS[byte0 >>> 4 & 0xf];    //    5
+            str[k++] = MD5_CHARS[byte0 & 0xf];   //   F
+        }
+        //返回经过加密后的字符串
+        return new String(str);
     }
 
     private static MD5 instance;
--- src/main/java/com/charles/utils/MD5Test.java
+++ src/main/java/com/charles/utils/MD5Test.java
@@ -5,9 +5,17 @@ import java.security.NoSuchAlgorithmException;
 public class MD5Test {
 
     public static void main(String args[]) throws NoSuchAlgorithmException {
-        String str = "{\"byFields\":[\"ES_EPG_CODE\",\"P_MARK\"],\"filters\":{\"conditions\":[{\"field\":\"ES_EPG_CODE\",\"function\":\"notEqual\",\"params\":[\"线路2\"]}],\"relation\":\"and\"},\"fromDate\":\"2017-04-01\",\"measures\":[{\"aggregator\":\"general\",\"eventName\":\"click\"}],\"project\":\"PKIPTV\",\"toDate\":\"2017-04-20\",\"unit\":\"day\"}";
-        // String str = "8a683566bcc7801226b3d8b0cf35fd97";
-        System.out.println(MD5.getInstance().getMD5(str));
-        System.out.println(CryptUtils.md5(str));
+        String str1 = "{\"byFields\":[\"ES_EPG_CODE\",\"P_MARK\"],\"filters\":{\"conditions\":[{\"field\":\"ES_EPG_CODE\",\"function\":\"notEqual\",\"params\":[\"线路2\"]}],\"relation\":\"and\"},\"fromDate\":\"2017-04-01\",\"measures\":[{\"aggregator\":\"general\",\"eventName\":\"click\"}],\"project\":\"PKIPTV\",\"toDate\":\"2017-04-20\",\"unit\":\"day\"}";
+        String str2 = "{\"byFields\":[\"P_TYPE\",\"P_MARK\"],\"filters\":{\"conditions\":[{\"field\":\"P_TYPE\",\"function\":\"notEqual\",\"params\":[\"线路4\"]}],\"relation\":\"and\"},\"fromDate\":\"2017-04-01\",\"measures\":[{\"aggregator\":\"general\",\"eventName\":\"click\"}],\"project\":\"PKIPTV\",\"toDate\":\"2017-04-19\",\"unit\":\"day\"}";
+        String str3 = "8a683566bcc7801226b3d8b0cf35fd97";
+        compare(str1, str2, str3);
+    }
+
+    public static void compare(String... strArr) throws NoSuchAlgorithmException {
+        for (String str : strArr) {
+            System.out.println("-------------------------------");
+            System.out.println(MD5.getInstance().getMD5(str));
+            System.out.println(CryptUtils.md5(str));
+        }
     }
 }
