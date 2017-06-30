905b753b7b0bbc6897db710f89a7a68766c2ad3e
Charles <hub@pukkasoft.cn>
2017-06-30 14:24:47 +0800

    加入发邮件

---
+++ src/main/java/com/charles/lib/email/JavaMail.java
@@ -0,0 +1,83 @@
+package com.charles.lib.email;
+
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+import static org.apache.commons.lang3.CharEncoding.UTF_8;
+
+public class JavaMail {
+    // public static String senderEmailAccount = "mrcharleshu@163.com";
+    // public static String senderEmailPassword = "fg3gmW7ambs2eUW9";
+    public static String senderEmailAccount = "huijiating@greatv.cn";
+    public static String senderEmailPassword = "123.Com";
+    // 收件人邮箱（替换为自己知道的有效邮箱）
+    public static String receiverEmailAccount = "hub@pukkasoft.cn";
+
+    private static final String MAIL_SMTP_HOST = "smtp.exmail.qq.com";
+    // private static final String MAIL_SMTP_HOST= "smtp.163.com";
+    private static final String MAIL_CONTENT_TYPE = "text/html;charset=UTF-8";
+
+    public static void main(String[] args) throws Exception {
+        // 1. 创建一封邮件
+        Properties props = new Properties();                 // 用于连接邮件服务器的参数配置（发送邮件时才需要用到）
+        props.setProperty("mail.transport.protocol", "smtp");// 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", MAIL_SMTP_HOST); // 发件人的邮箱的 SMTP 服务器地址
+        props.setProperty("mail.smtp.auth", "true");
+        Session session = Session.getDefaultInstance(props); // 根据参数配置，创建会话对象（为了发送邮件准备的）
+        session.setDebug(true);
+
+        // 3. 创建一封邮件
+        MimeMessage message = createMimeMessage(session, senderEmailAccount, receiverEmailAccount);
+        // 4. 根据 Session 获取邮件传输对象
+        Transport transport = session.getTransport();
+        // 5. 使用 邮箱账号 和 密码 连接邮件服务器, 这里认证的邮箱必须与 message 中的发件人邮箱一致, 否则报错
+        //
+        //    PS_01: 成败的判断关键在此一句, 如果连接服务器失败, 都会在控制台输出相应失败原因的 log,
+        //           仔细查看失败原因, 有些邮箱服务器会返回错误码或查看错误类型的链接, 根据给出的错误
+        //           类型到对应邮件服务器的帮助网站上查看具体失败原因。
+        //
+        //    PS_02: 连接失败的原因通常为以下几点, 仔细检查代码:
+        //           (1) 邮箱没有开启 SMTP 服务;
+        //           (2) 邮箱密码错误, 例如某些邮箱开启了独立密码;
+        //           (3) 邮箱服务器要求必须要使用 SSL 安全连接;
+        //           (4) 请求过于频繁或其他原因, 被邮件服务器拒绝服务;
+        //           (5) 如果以上几点都确定无误, 到邮件服务器网站查找帮助。
+        //
+        //    PS_03: 仔细看log, 认真看log, 看懂log, 错误原因都在log已说明。
+        transport.connect(senderEmailAccount, senderEmailPassword);
+        // 6. 发送邮件, 发到所有的收件地址, message.getAllRecipients() 获取到的是在创建邮件对象时添加的所有收件人, 抄送人, 密送人
+        transport.sendMessage(message, message.getAllRecipients());
+        // 7. 关闭连接
+        transport.close();
+    }
+
+    /**
+     * 创建一封只包含文本的简单邮件
+     * @param session       和服务器交互的会话
+     * @param senderEmail   发件人邮箱
+     * @param receiverEmail 收件人邮箱
+     * @return
+     * @throws Exception
+     */
+    public static MimeMessage createMimeMessage(Session session, String senderEmail, String receiverEmail) throws Exception {
+        // 1. 创建一封邮件
+        MimeMessage message = new MimeMessage(session);
+        // 2. From: 发件人
+        message.setFrom(new InternetAddress(senderEmail, "某宝网", UTF_8));
+        // 3. To: 收件人（可以增加多个收件人、抄送、密送）
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiverEmail, "XX用户", UTF_8));
+        // 4. Subject: 邮件主题
+        message.setSubject("打折钜惠", UTF_8);
+        // 5. Content: 邮件正文（可以使用html标签）
+        message.setContent("新订单产生，订单号：123456789。请尽快处理！", MAIL_CONTENT_TYPE);
+        // 6. 设置发件时间
+        message.setSentDate(new Date());
+        // 7. 保存设置
+        message.saveChanges();
+        return message;
+    }
+}
