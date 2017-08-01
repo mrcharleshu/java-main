4b9cb76956e05b7b8728deeff1ad624b54bd61f3
Charles <mrcharleshu@gmail.com>
2017-08-01 18:22:34 +0800

    新增二叉树

--- src/main/java/com/charles/algorithm/BinaryTree.java
+++ src/main/java/com/charles/algorithm/BinaryTree.java
@@ -276,7 +276,7 @@ public class BinaryTree {
     }
 
     public static void main(String[] args) {
-        // 4度满二叉树
+        // 满二叉树
         // int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         // 完全二叉树（只要有8就可以，9-15任选）
         // int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
