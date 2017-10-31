9f35f312bbe3673e565738e1020f266b29f768aa
Charles <hubin@nanchao.org>
2017-10-31 17:47:30 +0800

    add NumberJudge

---
+++ src/main/java/com/charles/common/number/NumberJudge.java
@@ -0,0 +1,26 @@
+package com.charles.common.number;
+
+import java.util.regex.Pattern;
+
+public class NumberJudge {
+    public static void main(String[] args) {
+        System.out.println(isStrNumber1("123"));
+        System.out.println(isStrNumber1("code"));
+        System.out.println(isStrNumber2("123"));
+        System.out.println(isStrNumber2("code"));
+    }
+
+    private static boolean isStrNumber1(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isStrNumber2(String str) {
+        Pattern pattern = Pattern.compile("^[0-9]*$");
+        return pattern.matcher(str).matches();
+    }
+}
