dc3db2a534f932aab69c7febd83995d9796a384a
Charles <mrcharleshu@gmail.com>
2017-11-09 18:55:31 +0800

    complex sort

--- src/main/java/com/charles/common/sort/ComparatorSort.java
+++ src/main/java/com/charles/common/sort/ComparatorSort.java
@@ -7,13 +7,15 @@ import java.util.List;
 import static com.charles.utils.LineSeparators.hyphenSeparator;
 
 public class ComparatorSort {
+    
     public static void main(String[] args) {
         List<ComparatorStudent> list = new ArrayList<>();// Java 7的钻石语法(构造器后面的尖括号中不需要写类型)
         list.add(new ComparatorStudent("Hao LUO", 33, 175));
         list.add(new ComparatorStudent("XJ WANG", 32, 168));
         list.add(new ComparatorStudent("Bob YANG", 22, 183));
         list.add(new ComparatorStudent("Bruce LEE", 33, 174));
-        list.add(new ComparatorStudent("Charles", 22, 177));
+        list.add(new ComparatorStudent("Charles", 32, 177));
+        list.add(new ComparatorStudent("Sonya", 25, 162));
 
         // 通过sort方法的第二个参数传入一个Comparator接口对象
         // 相当于是传入一个比较对象大小的算法到sort方法中
@@ -21,9 +23,14 @@ public class ComparatorSort {
         // 因此要将一个算法传入一个方法中唯一的选择就是通过接口回调
         // Collections.sort(list, Comparator.comparing(ComparatorStudent::getName));
         // list.sort(Comparator.comparing(ComparatorStudent::getAge));
-        list.sort(Comparator.comparing(ComparatorStudent::getAge));
-        list.forEach(System.out::println);
+        // list.forEach(System.out::println);
         hyphenSeparator();
+        // complexSort(list);
+        list.sort(Comparator.comparing(ComparatorStudent::getAge).thenComparing(ComparatorStudent::getHeight));
+        list.forEach(System.out::println);
+    }
+
+    private static void complexSort(final List<ComparatorStudent> list) {
         list.sort((o1, o2) -> {
             if (o1.getAge() > o2.getAge()) {
                 return 1;
@@ -33,7 +40,6 @@ public class ComparatorSort {
                 return -1;
             }
         });
-        list.forEach(System.out::println);
     }
 
     private static class ComparatorStudent {
